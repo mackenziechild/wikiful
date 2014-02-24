12154e3a85b3f6b165216532a633e9f532b2ce1f
Mackenzie Child <mackenzie.child@gmail.com>
2014-02-24 06:27:42 -0700

    initial new and create action and new view for Articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,9 +9,20 @@ class ArticlesController < ApplicationController
 	end
 
 	def new
+		@article = Article.new
 	end
 
 	def create
+		@article = Article.new(article_params)
+		if @article.save
+			redirect_to @article
+		else
+			render "new"
+		end
 	end
 
+	private
+		def article_params
+			params.require(:article).permit(:title, :content, :category_ids => [])
+		end
 end
