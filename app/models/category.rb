5c44f73f4a587af91f2f7f4fff181dfd0adb9929
Mackenzie Child <mackenzie.child@gmail.com>
2014-02-23 18:56:46 -0700

    fix validation in Category model

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,5 @@
 class Category < ActiveRecord::Base
 	has_many :article_categories
 	has_many :articles, through: :article_categories
-	validate_uniqueness_of :name
+	validates_uniqueness_of :name
 end
