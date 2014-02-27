1f7e3c964593ae0eb3c8c0980e3f9a661d0451c6
Mackenzie Child <mackenzie.child@gmail.com>
2014-02-27 06:38:05 -0700

    complete site structure css

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,11 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_filter :get_categories
+
+  private
+  def get_categories
+  	@categories = Category.all.order(:name)
+  end
+
 end
